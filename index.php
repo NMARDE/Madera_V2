744ab719154eb2275f8702669eb89f95acf4b537
NMARDE <49159478+NMARDE@users.noreply.github.com>
2020-04-07 00:11:21 +0200

    Fin front accueil

--- index.php
+++ index.php
@@ -2,36 +2,44 @@
 require("./header.php")
 ?>
 
+<div class="hero-wrap js-fullheight" style="background-image: url('image/bois_accueil.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+        </div>
+      </div>
+    </div>
+
 <section class="ftco-section ftco-services">
     	<div class="container">
     		<div class="row">
           <div class="col-md-4 d-flex services align-self-stretch px-4 ftco-animate">
             <div class="d-block services-wrap text-center">
-              <div class="img" style="background-image: url(image/simuler_maison.png);"></div>
+              <div class="img" style="background-image: url(image/simuler_maison.jpg);"></div>
               <div class="media-body py-4 px-3">
                 <h3 class="heading">Simuler une maison</h3>
                 <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                <p><a href="#" class="btn btn-primary">Read more</a></p>
+                <p><a href="#" class="btn btn-primary">Simuler</a></p>
               </div>
             </div>      
           </div>
           <div class="col-md-4 d-flex services align-self-stretch px-4 ftco-animate">
             <div class="d-block services-wrap text-center">
-              <div class="img" style="background-image: url(image/services-2.jpg);"></div>
+              <div class="img" style="background-image: url(image/client.jpg);"></div>
               <div class="media-body py-4 px-3">
                 <h3 class="heading">Rechercher un client</h3>
                 <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                <p><a href="#" class="btn btn-primary">Read more</a></p>
+                <p><a href="#" class="btn btn-primary">Rechercher</a></p>
               </div>
             </div>    
           </div>
           <div class="col-md-4 d-flex services align-self-stretch px-4 ftco-animate">
             <div class="d-block services-wrap text-center">
-              <div class="img" style="background-image: url(image/services-3.jpg);"></div>
+              <div class="img" style="background-image: url(image/projet.png);"></div>
               <div class="media-body py-4 px-3">
-                <h3 class="heading">Rechercher un produit</h3>
+                <h3 class="heading">Rechercher un projet</h3>
                 <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                <p><a href="#" class="btn btn-primary">Read more</a></p>
+                <p><a href="#" class="btn btn-primary">Rechercher</a></p>
               </div>
             </div>      
           </div>
