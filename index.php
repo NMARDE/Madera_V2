7f45bc551b68020a3bf5230a7743abc44d331256
NMARDE <49159478+NMARDE@users.noreply.github.com>
2020-04-06 22:17:07 +0200

    Ajout complément js et css

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+require("./header.php")
+?>
+
+<section class="ftco-section ftco-services">
+    	<div class="container">
+    		<div class="row">
+          <div class="col-md-4 d-flex services align-self-stretch px-4 ftco-animate">
+            <div class="d-block services-wrap text-center">
+              <div class="img" style="background-image: url(image/simuler_maison.png);"></div>
+              <div class="media-body py-4 px-3">
+                <h3 class="heading">Simuler une maison</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <p><a href="#" class="btn btn-primary">Read more</a></p>
+              </div>
+            </div>      
+          </div>
+          <div class="col-md-4 d-flex services align-self-stretch px-4 ftco-animate">
+            <div class="d-block services-wrap text-center">
+              <div class="img" style="background-image: url(image/services-2.jpg);"></div>
+              <div class="media-body py-4 px-3">
+                <h3 class="heading">Rechercher un client</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <p><a href="#" class="btn btn-primary">Read more</a></p>
+              </div>
+            </div>    
+          </div>
+          <div class="col-md-4 d-flex services align-self-stretch px-4 ftco-animate">
+            <div class="d-block services-wrap text-center">
+              <div class="img" style="background-image: url(image/services-3.jpg);"></div>
+              <div class="media-body py-4 px-3">
+                <h3 class="heading">Rechercher un produit</h3>
+                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <p><a href="#" class="btn btn-primary">Read more</a></p>
+              </div>
+            </div>      
+          </div>
+        </div>
+    	</div>
+    </section>
+
+<?php
+require("./footer.php")
+?>
