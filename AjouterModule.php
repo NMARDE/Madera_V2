4643e0ac959598cf2106cd6891346d647abe5087
NMARDE <49159478+NMARDE@users.noreply.github.com>
2020-04-19 17:24:06 +0200

    Front module

--- AjouterModule.php
+++ AjouterModule.php
@@ -5,32 +5,110 @@ if(!isset($_POST['Module'])){
 
 ?>
 
+
 <html>
-<form method="post" action="AjouterModule.php">
+<head>
+    <title>MaderaApp - Selection de la gamme</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="./css/animate.css">
+    
+    <link rel="stylesheet" href="./css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="./css/magnific-popup.css">
+
+    <link rel="stylesheet" href="./css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="./css/jquery.timepicker.css">
+
+    <link rel="stylesheet" href="./css/flaticon.css">
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>
+
+  <?php 
+  require("navbar.php");
+  ?>
+
+<div class="hero-wrap js-fullheight" style="background-image: url('image/bois_accueil.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+        <h1 class="mb-4-selection">Ajoutez des modules comme bon vous semble...</h1>
+        </div>
+      </div>
+	</div>
+
+
+<div class="container">
+    <div class="card card-container">
+	<form method="post" class="form-signin" action="AjouterModule.php">
 <?php
-echo' <select id="Module" name="Module" id="Module">';
+echo' <select id="Module" class="form-control" name="Module" id="Module">';
 $arrayModule=listeModule();
 if(!empty($arrayModule)){
 	foreach($arrayModule as $unModule){
 		echo '<option value="'.$unModule.'">'.$unModule.'</option>';
 	}
 }
-echo'</select>';
+echo'</select></br>';
 ?>
-<input type="submit" value="ok"/>
-</form>
-</html>
+<input type="submit" class="btn btn-primary" value="Selectionner"/>
 <?php
 }else{
 	if(!isset($_POST['CaracCount'])){
 	echo'<html>
-	<form method="post" action="AjouterModule.php"><br>';
+	<head>
+		<title>MaderaApp - Selection de la gamme</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+	
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	 
+		<link rel="stylesheet" href="./css/animate.css">
+		
+		<link rel="stylesheet" href="./css/owl.carousel.min.css">
+		<link rel="stylesheet" href="./css/owl.theme.default.min.css">
+		<link rel="stylesheet" href="./css/magnific-popup.css">
+	
+		<link rel="stylesheet" href="./css/bootstrap-datepicker.css">
+		<link rel="stylesheet" href="./css/jquery.timepicker.css">
+	
+		<link rel="stylesheet" href="./css/flaticon.css">
+		<link rel="stylesheet" href="./css/style.css">
+	  </head>
+	  <body>';
+	
+
+	  require("navbar.php");
+
+echo'	<div class="hero-wrap js-fullheight" style="background-image: url(\'image/bois_accueil.jpg\');" data-stellar-background-ratio="0.5">
+		  <div class="overlay"></div>
+		  <div class="container">
+			<div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
+			<h1 class="mb-4">Ajoutez des modules comme bon vous semble...</h1>
+			</div>
+		  </div>
+		</div>
+	
+	
+	<div class="container">
+		<div class="card card-container">
+	<form method="post" class="form-signin" action="AjouterModule.php"><br>';
 	if(isset($_POST['NomModule'])){
 		echo "Nom du module: ".$_POST['NomModule'];
 	}else{
 		echo "Nom du module: ".$_POST['Module'];
 	}
-	echo'<input type="text" name="tailleModule" id="tailleModule" placeholder="Taille du module"/><br>';
+	echo'<input type="text" class="form-control" name="tailleModule" id="tailleModule" placeholder="Taille du module en m²"/><br>';
 	$listeCarac=array();
 	$listeCarac=listeCarac($_POST['Module']);
 	$listeValCarac=array();
@@ -38,7 +116,7 @@ echo'</select>';
 		if($i%2 != 1){
 		$listeValCarac=listeValCarac($listeCarac[$i]);
 		echo $listeCarac[$i+1];
-		echo "<select id='Carac".$i."' name='Carac".$i."'>";
+		echo "<select class='form-control' id='Carac".$i."' name='Carac".$i."'>";
 		foreach($listeValCarac as $oneValCarac){
 			echo "<option value=".$oneValCarac.">".$oneValCarac."</option>";
 		}
@@ -47,8 +125,8 @@ echo'</select>';
 		}
 	}
 	echo'<input type="hidden" name="NomModule" id="NomModule" value="'.$_POST['Module'].'">
-	<input type="hidden" name="Module" id="Module" value="'.$_POST['Module'].'"/>
-	<input type="hidden" name="CaracCount" id="CaracCount" value='.count($listeCarac).'/><input type="submit" value="ok"';
+	<input type="hidden" name="Module" id="Module" value="'.$_POST['Module'].'"/></br>
+	<input type="hidden" name="CaracCount" id="CaracCount" value='.count($listeCarac).'/><input type="submit" class="btn btn-primary btn-block" value="Valider"';
 	echo'</html>';
 	}else{
 		$listeCarac=array();
