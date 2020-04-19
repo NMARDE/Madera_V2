fa9abd5cf1a02cbb2d7fe9aeb03b0d8722d10a8d
NMARDE <49159478+NMARDE@users.noreply.github.com>
2020-04-19 14:50:32 +0200

    SelectiomGamme et Module

--- DetailsMaison.php
+++ DetailsMaison.php
@@ -1,7 +1,36 @@
 <?php 
 session_start();
 include('DAO.php');
-echo'<html>
+echo'
+<html>
+<head>
+    <title>Nommer votre projet</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="./css/animate.css">
+    
+    <link rel="stylesheet" href="./css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="./css/magnific-popup.css">
+
+    <link rel="stylesheet" href="./css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="./css/jquery.timepicker.css">
+
+    <link rel="stylesheet" href="./css/flaticon.css">
+    <link rel="stylesheet" href="./css/style.css">
+  </head>
+  <body>';
+
+  require('navbar.php');
+
+  echo'
+<div class="container">
 <form method="post" action="Recapitulatif.php">
 <input type="text" placeholder="Nom du plan" name="NomPlan"/>
 <input type="radio" name="Gamme" value="Standard">Standard</input>
